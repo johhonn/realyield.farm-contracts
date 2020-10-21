8802a3bfe25ad91d56494f42a0f9744374ced935
johhonn <35551051+johhonn@users.noreply.github.com>
2020-10-21 18:56:07 -0400

    working update

--- contracts/reoccuringRandom.sol
+++ contracts/reoccuringRandom.sol
@@ -10,31 +10,39 @@ contract reocurringRandom is ChainlinkClient, Ownable {
     uint256 public lotteryId;
     address public RVF;
     uint256 public interval;
-    
+    bool public cancelled;
    
     
-    // 0.1 LINK
-    uint256 public ORACLE_PAYMENT = 100000000000000000;
+    // 1 LINK
+    uint256 ORACLE_PAYMENT = 1 * 10 ** 18;
     // Alarm stuff
-    address CHAINLINK_ALARM_ORACLE = 0xc99B3D447826532722E41bc36e644ba3479E4365;
-    bytes32 CHAINLINK_ALARM_JOB_ID = "2ebb1c1a4b1e4229adac24ee0b5f784f";
-    
+    address CHAINLINK_ALARM_ORACLE = 0x2f90A6D021db21e1B2A077c5a37B3C7E75D15b7e;
+    bytes32 CHAINLINK_ALARM_JOB_ID = "a7ab70d561d34eb49e9b1612fd2e044b";
+  
     constructor(address _rvf, uint _interval) public
     {
         setPublicChainlinkToken();
         lotteryId = 1;
-        state = RSTATE.CLOSED;
+        //state = RSTATE.CLOSED;
         RVF=_rvf;
-        start_new_lottery(_interval);
+    
+        interval=_interval;
     }
-
+  /**
+   * @notice Allows the owner to withdraw any LINK balance on the contract
+   */
+  function withdrawLink() public onlyOwner {
+    LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
+    require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
+  }
   
     
   function start_new_lottery(uint256 duration) internal {
-    require(state == RSTATE.CLOSED, "can't start a new lottery yet");
-    state = RSTATE.OPEN;
+   // require(state == RSTATE.CLOSED, "can't start a new lottery yet");
+    //require(cancelled==false,"cancelled must be false ");
+   // state= RSTATE.OPEN;
     Chainlink.Request memory req = buildChainlinkRequest(CHAINLINK_ALARM_JOB_ID, address(this), this.fulfill_alarm.selector);
-    req.addUint("until", now + duration);
+    req.addUint("until", now + duration* 1 seconds);
     sendChainlinkRequestTo(CHAINLINK_ALARM_ORACLE, req, ORACLE_PAYMENT);
   }
   
@@ -42,15 +50,25 @@ contract reocurringRandom is ChainlinkClient, Ownable {
     public
     recordChainlinkFulfillment(_requestId)
       {
-        require(state == RSTATE.OPEN, "The lottery hasn't even started!");
+        //require(state == RSTATE.OPEN, "The lottery hasn't even started!");
         // add a require here so that only the oracle contract can
         // call the fulfill alarm method
-        state = RSTATE.CALCULATING_WINNER;
+       // state = RSTATE.CALCULATING_WINNER;
         lotteryId = lotteryId + 1;
-        VRFrandom(RVF).getRandom(lotteryId);
+        VRFrandom(RVF).getRandomNumber(lotteryId);
+       // state= RSTATE.CLOSED;
         start_new_lottery(interval);
     }
-    function cancelRequests() public  onlyOwner(){
-        state = RSTATE.CLOSED;  
+    function testVRF() public  onlyOwner(){
+       VRFrandom(RVF).getRandomNumber(now);
     }
-    
+    function cancelRequests() public  onlyOwner(){
+          cancelled=true;
+    }
+    function setInterval(uint i) public onlyOwner(){
+        interval=i;
+    }
+    function initialize() public onlyOwner(){
+      start_new_lottery(interval);
+    }
+}
